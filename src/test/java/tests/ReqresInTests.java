0e3d6300f1220e553ea99d4a05349f152adcd1c5
mikhail.matskevich <mih198830@gmail.com>
2022-07-21 20:01:32 +0400

    Tests added 5

--- src/test/java/tests/ReqresInTests.java
+++ src/test/java/tests/ReqresInTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.http.ContentType.JSON;
-import static io.restassured.http.ContentType.XML;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 
@@ -12,7 +12,6 @@ public class ReqresInTests {
     @Test
     void checkIfNumberOfPageEqualTwo(){
         given()
-                .log().all()
                 .when()
                 .get("https://reqres.in/api/users")
                 .then()
@@ -67,7 +66,7 @@ public class ReqresInTests {
 
     @Test
     void apiRegisterTestTokenExist(){
-            String body = "{ \"email\": \"eve.holt@reqres.in\"}";
+            String body = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
 
             given()
                     .contentType(JSON)
@@ -75,7 +74,7 @@ public class ReqresInTests {
                     .when()
                     .post("https://reqres.in/api/login")
                     .then()
-                    .statusCode(400)
-                    .body("error", is("Missing password"));
+                    .statusCode(200)
+                    .body("token", equalTo("QpwL5tke4Pnpja7X4"));
         }
 }
